70da072c8a0a1ed118ebb6e4fc4dd7c2a961c48c
ppj-555 <Prasanna.Prakhar@shell.com>
2023-04-16 19:18:53 +0530

    linking microservice with FE

--- userapp/src/main/java/com/userapp/userapp/ConnectionController.java
+++ userapp/src/main/java/com/userapp/userapp/ConnectionController.java
@@ -1,10 +1,12 @@
 package com.userapp.userapp;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ConnectionController {
+    @CrossOrigin
     @RequestMapping("/api/users")
     public String index(){
         return "The microservice created and linked";
--- web/src/App.js
+++ web/src/App.js
@@ -1,9 +1,22 @@
 import "./App.css";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [msg, setMsg] = useState(null);
+
+  const getData = async () => {
+    const response = await axios.get("http://localhost:8080/api/users");
+    setMsg(response.data);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
   return (
     <div className="App">
-      <header>UserApp</header>
+      <h3>UserApp</h3>
+      <h5>{msg}</h5>
     </div>
   );
 }
