04448ae471f959fca4c2ac519f974db9c4aa2540
ppj-555 <Prasanna.Prakhar@shell.com>
2023-04-16 20:39:10 +0530

    node server added

---
+++ node-server/index.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var app = express();
+
+var cors = require("cors");
+
+app.use(cors());
+
+var port = 5000;
+
+// var routes = require("./api/routes");
+
+// routes(app);
+
+app.get("/", (req, res) => {
+  res.send("Linking with node server");
+});
+
+// server.listen(5000); //3 - listen for any incoming requests
+
+console.log("Node.js web server at port 5000 is running..");
+
+app.listen(port, () => {
+  console.log("Server started on port: " + port);
+});
--- web/src/App.js
+++ web/src/App.js
@@ -4,19 +4,29 @@ import React, { useState, useEffect } from "react";
 
 function App() {
   const [msg, setMsg] = useState(null);
+  const [nodeMsg, setNodeMsg] = useState(null);
 
-  const getData = async () => {
+  const getDataFromJavaServer = async () => {
     const response = await axios.get("http://localhost:8080/api/users");
     setMsg(response.data);
   };
 
+  const getDataFromNodeServer = async () => {
+    const response = await axios.get("http://localhost:5000/");
+    setNodeMsg(response.data);
+  };
+
   useEffect(() => {
-    getData();
+    getDataFromJavaServer();
+    getDataFromNodeServer();
   }, []);
   return (
     <div className="App">
       <h3>UserApp</h3>
+      <br />
       <h5>{msg}</h5>
+      <br />
+      <h5>{nodeMsg}</h5>
     </div>
   );
 }
