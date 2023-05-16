81de2b5e2cec90206ea54bf4df476cdefc01751e
Shwetanshu89 <shekharahwet45@gmail.com>
2023-05-16 13:11:20 +0530

    div-structures

--- web/src/pages/Home.js
+++ web/src/pages/Home.js
@@ -4,26 +4,44 @@ import { List } from "../components/Lists";
 import { HEADING } from "../components/constants";
 import { Records } from "../components/Records";
 import { useState } from "react";
+import "./Home.css";
 
 export const Home = () => {
   const [isUserCreated, setIsIUserCreated] = useState(false);
 
   return (
+    // <div>
+    //   <div>
+    //     <div>
+    //       <Heading heading={HEADING} />
+    //     </div>
+    //     <div>
+    //       <Fields setIsIUserCreated={setIsIUserCreated} />
+    //     </div>
+    //   </div>
+    //   <div>
+    //     <div>
+    //       <List />
+    //     </div>
+    //   </div>
+    //   <div>
+    //     <div style={{ margin: "120px" }}>
+    //       <Records isUserCreated={isUserCreated ? isUserCreated : false} />
+    //     </div>
+    //   </div>
+    // </div>
+
+    // <div className="form-section">
+
+    // </div>
     <div>
-      <div>
-        <div>
-          <Heading heading={HEADING} />
-        </div>
-        <div>
-          <Fields setIsIUserCreated={setIsIUserCreated} />
-        </div>
+      <div className="Heading-section">
+        <Heading heading={HEADING} />
       </div>
-      <div>
-        <div>
-          <List />
-        </div>
+      <div className="Form-section">
+        <Fields setIsIUserCreated={setIsIUserCreated} />
       </div>
-      <div>
+      <div className="List-section">
         <div style={{ margin: "120px" }}>
           <Records isUserCreated={isUserCreated ? isUserCreated : false} />
         </div>
